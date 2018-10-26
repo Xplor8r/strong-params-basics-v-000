8990a6fa3501015d744eefdc56da0b4f3a0db96c
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-26 18:21:13 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,11 +19,15 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title))
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	def post_params(*args)
+  	params.require(:post).permit(*args)
 	end
 end
